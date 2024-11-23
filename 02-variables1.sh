28b3021089ce2410fe18ff6cc6f41641d849cea3
Chinmai <chinmaidevi2000.com>
2024-11-23 17:55:15 +0530

    commands of git

--- 02-variables1.sh
+++ 02-variables1.sh
@@ -2,8 +2,14 @@
 
 #assinging valus satically
 
-echo "Chinmai: Hi Chinni, How are you?"
-echo "Chinni: Hi Chinmai, I am good, how are you?"
-echo "Chinmai: I'm good too!, how's your work?"
-echo "Chinni: Yeah its going good"
+person1=Chinmai
+person2=Sudheer
+
+echo "$person1: Hi $person2, How are you?"
+echo "$person2: Hi $person1, I am good, how are you?"
+echo "$person1: I'm good too!, how's your work?"
+echo "$person2: Yeah its going good"
+
+
+
 
