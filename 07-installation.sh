53766abe3a38c8f13e3b72b29cef88f1146f676b
Chinmai <chinmaidevi2000.com>
2024-11-23 21:33:39 +0530

    shell commands

---
+++ 07-installation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if ( $userid -ne 0)
+then 
+    echo "Please run the script with user access"
+else 
+    echo "Yay you are super user"
+
+fi
+
+dnf install mysql -y
