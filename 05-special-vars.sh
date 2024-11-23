93145cc3e1f2a142bd381bd807a4a131258786d5
Chinmai <chinmaidevi2000.com>
2024-11-23 20:07:07 +0530

    shell commands

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
 echo "@ :is used for all the variables -> $@ "
+echo "# :is used to show no. of variables -> $#"
+echo "0 :is used to print script name -> $0"
+echo "PWD :is used to print current working directory (pwd should be in caps) -> $PWD"
+echo "HOME :is used to show home directory -> $HOME"
+echo "HOSTNAME :print the hostname -> $HOSTNAME"
+echo "$ : prints the process id of current shell script -> $$"
+echo "! :print the process id of last backround command -> $!"
+
+#to make any process as background use '&'
+
+sleep 10 &
