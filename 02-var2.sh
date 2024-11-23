64a8ee1f7b09743bc0f2c996c05a7922545b0e28
Chinmai <chinmaidevi2000.com>
2024-11-23 17:57:54 +0530

    commands of git

---
+++ 02-var2.sh
@@ -0,0 +1,7 @@
+person1=$1
+person2=$2
+
+echo "$person1: Hi $person2, How are you?"
+echo "$person2: Hi $person1, I am good, how are you?"
+echo "$person1: I'm good too!, how's your work?"
+echo "$person2: Yeah its going good"
