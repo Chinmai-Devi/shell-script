23c3b081665f98a2ffa65a6618eaba3c10241fee
Chinmai <chinmaidevi2000.com>
2024-11-23 21:12:27 +0530

    shell commands

--- 06-conditions.sh
+++ 06-conditions.sh
@@ -9,3 +9,12 @@ then
 else
     echo "$n1 is less than $n2"
 fi
+
+
+# Operators-> -lt, -eq, -ge, -le
+# COnditions -> 
+#if statement
+#if-else statement
+#if..elif..else..fi statement (Else If ladder)
+#if..then..else..if..then..fi..fi..(Nested if)
+#switch statement 
