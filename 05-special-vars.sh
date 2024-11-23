444a30ae57af1ee9c281eefd5f8c578ae60d688a
Chinmai <chinmaidevi2000.com>
2024-11-23 20:07:45 +0530

    shell commands

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -7,8 +7,9 @@ echo "PWD :is used to print current working directory (pwd should be in caps) ->
 echo "HOME :is used to show home directory -> $HOME"
 echo "HOSTNAME :print the hostname -> $HOSTNAME"
 echo "$ : prints the process id of current shell script -> $$"
-echo "! :print the process id of last backround command -> $!"
 
 #to make any process as background use '&'
 
 sleep 10 &
+
+echo "! :print the process id of last backround command -> $!"
