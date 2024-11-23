7600bc630971197d632740b629641c799b7da47c
Chinmai <chinmaidevi2000.com>
2024-11-23 19:45:59 +0530

    shell commands

--- 04-operators.sh
+++ 04-operators.sh
@@ -3,7 +3,14 @@
 a=$1
 b=$2
 
-sum=$(($a+$b))
 
+#here $ is most imp, if we gice $ then only shell is considering that is a value
+sum=$(($a+$b))
+sub=$(($a-$b))
+mukl=$(($a*$b))
+div=$(($a/$b))
 
 echo "sum is : $sum"
+echo "sum is : $sub"
+echo "sum is : $mul"
+echo "sum is : $div"
