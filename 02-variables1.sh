bb427f949e09ddb578e58be5632d2d497966292e
Chinmai <chinmaidevi2000.com>
2024-11-23 16:28:46 +0530

    added dummy file

---
+++ 02-variables1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#assinging valus satically
+
+echo "Chinmai: Hi Chinni, How are you?"
+echo "Chinni: Hi Chinmai, I am good, how are you?"
+echo "Chinmai: I'm good too!, how's your work?"
+echo "Chinni: Yeah its going good"
+
