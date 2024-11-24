f2a96f76a24aa9b74878b0cd1dcbdf8a20fa6827
Chinmai <chinmaidevi2000.com>
2024-11-24 17:31:30 +0530

    some changes

---
+++ 09-logs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+userid=$(id -u) # means here we are giving a command's o/p to a variable dynamically
+
+timestamp=$(date +%F-%H-%M-%S)
+scriptname=$(echo $0 | cut -d "." -f1)
+
+logfile=/tmp/$scriptname-$timestamp.log
+
+
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+        echo "$2 Success"
+    else 
+        echo "$2 Falure"
+    fi
+}
+
+if [ $userid -eq 0 ]
+then 
+    echo "Yay! you have super user access so proceeding with installations"
+else
+    echo "Please run with super access"
+    exit 1
+fi
+
+
+
+dnf install mysql -y
+
+validate $? "MySQL Installation"
+
+dnf install git -y 
+ 
+validate $? "git installation"
