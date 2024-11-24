58d66a2d104e59789f70b42d36cd074246342dac
Chinmai <chinmaidevi2000.com>
2024-11-24 18:01:48 +0530

    some changes

--- 10-colors.sh
+++ 10-colors.sh
@@ -6,8 +6,8 @@ echo "All about colours"
 
 n="\e[0m"
 
-echo -e "This is \e[34m Clue color $n"
+echo -e "This is \e[34m Blue color $n"
 
-echo -e "This is \e[35m Purple"
+echo -e "This is \e[35m Purple $n"
 
 echo -e "Mix of colors is \e[34mSblue \e[32m Green \e[35m Purple"
