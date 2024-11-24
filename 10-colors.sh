55188e35c137d5993ea131ec3d0f322b23bb040e
Chinmai <chinmaidevi2000.com>
2024-11-24 17:52:41 +0530

    some changes

--- 10-colors.sh
+++ 10-colors.sh
@@ -9,4 +9,4 @@ echo -e "Groom is \e[34m Sudheer"
 
 echo -e "Bride is \e[35m Chinmai"
 
-echo -e "\e[34m Sudheer Kumar \e[32m Weds \e[35m Chinmai Devi"
+echo -e "\e[34mSudheer Kumar \e[32m Weds \e[35m Chinmai Devi"
