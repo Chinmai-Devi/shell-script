7c11a0a9e7c55fdc65372a147e05a2c21d30765d
Chinmai <chinmaidevi2000.com>
2024-11-24 14:01:57 +0530

    some changes

--- 07-installation.sh
+++ 07-installation.sh
@@ -5,7 +5,7 @@ userid=$(id -u)
 if [ $userid -ne 0 ]
 then 
     echo "Please run the script with user access"
-    exit 1
+    exit 1  #most imp - $? shows exit status - if true then it shows 0 otherswise 1-127
 else 
     echo "Yay you are super user"
 
