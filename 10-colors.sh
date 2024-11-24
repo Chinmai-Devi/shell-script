f06ada0d6633301087a336b53c3ffc725f8a9ec1
Chinmai <chinmaidevi2000.com>
2024-11-24 17:50:35 +0530

    some changes

---
+++ 10-colors.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "All about colours"
+
+#the syntac is \e[colorcodem    eg: \e[33m
+
+
+echo -e "Groom is \e[34m Sudheer"
+
+echo -e "Bride is \e[35m Chinmai"
+
+echo -e "\e[34m Sudheer Kumar \e[32m Weds \e[35m Chinmai Devi"
