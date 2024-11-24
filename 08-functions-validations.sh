a8d79a710cffd68c70cc79dc32ea030d5588d453
Chinmai <chinmaidevi2000.com>
2024-11-24 15:11:14 +0530

    some changes

---
+++ 08-functions-validations.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+validate(){
+    if [ $1 -eq 0 ]
+    then 
+        echo "Success"
+    else 
+        echo "Falure"
+    fi
+}
+
+if [ $? -eq 0 ]
+then 
+    echo "Yay! you have super user access so proceeding with installations"
+else
+    echo "Please run with super access"
+fi
+
+
+
+dnf install mysql -y
+
+validate $? "MySQL Installation"
+
+dnf install git -y 
+ 
+validate $? "git installation"
