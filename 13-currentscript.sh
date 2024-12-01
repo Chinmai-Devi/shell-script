dfbe40a3ec5479c332d0ff996e77a62b11a9cd8b
Chinmai <chinmaidevi2000.com>
2024-12-01 19:52:16 +0530

    some changes done

--- 13-currentscript.sh
+++ 13-currentscript.sh
@@ -6,7 +6,7 @@ echo "You are seeing the message before calling of other script: $greeting"
 echo "process id of current script: $$"
 
 #./14-otherscript.sh  1st way
-source ./14-otherscript.sh
+source ./14-otherscript.sh #2nd way
 
 echo "You are seeing the message after calling of other script: $greeting"
 
