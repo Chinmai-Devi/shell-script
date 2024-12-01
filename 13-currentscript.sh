b99d292a2aeef7f8357fd01a6dd11f6707badb05
Chinmai <chinmaidevi2000.com>
2024-12-01 19:49:52 +0530

    some changes done

--- 13-currentscript.sh
+++ 13-currentscript.sh
@@ -5,7 +5,8 @@ greeting="Hi Hello, I am Chinmai"
 echo "You are seeing the message before calling of other script: $greeting"
 echo "process id of current script: $$"
 
-./14-otherscript.sh
+#./14-otherscript.sh  1st way
+source ./14-otherscript.sh
 
 echo "You are seeing the message after calling of other script: $greeting"
 
